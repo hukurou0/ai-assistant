3191ab2fe924d8c9ef50774dffd0cc5dd97d538d
hukurou0 <junjun.0712.on@gmail.com>
2024-07-31 00:06:43 +0900

    fix SelectedTodos

--- front/src/components/SelectedTodos.tsx
+++ front/src/components/SelectedTodos.tsx
@@ -1,40 +1,41 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function SelectedTodos() {
-  return (
-    
-    <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
-        <li className="flex items-center space-x-3 rtl:space-x-reverse">
-            <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-            <span>Individual configuration</span>
-        </li>
-        <li className="flex items-center space-x-3 rtl:space-x-reverse">
-            <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-            <span>No setup, or hidden fees</span>
-        </li>
-        <li className="flex items-center space-x-3 rtl:space-x-reverse">
-            <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-            <span>Team size: <span className="font-semibold text-gray-900 dark:text-white">1 developer</span></span>
-        </li>
-        <li className="flex items-center space-x-3 rtl:space-x-reverse">
-            <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-            <span>Premium support: <span className="font-semibold text-gray-900 dark:text-white">6 months</span></span>
-        </li>
-        <li className="flex items-center space-x-3 rtl:space-x-reverse">
-            <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-            <span>Free updates: <span className="font-semibold text-gray-900 dark:text-white">6 months</span></span>
-        </li>
-    </ul>
+
+    const selectedTodos = [
+        {
+            id:"1",
+            name:"Vue JS",
+        },
+        {
+            id:"2",
+            name:"React",
+        },
+        {
+            id:"3",
+            name:"Angular",
+        },
+    ]
+
+  return ( 
+    <>
+        <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+            {selectedTodos.map((todo, index) => (
+                <>
+                <li key={index} className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                <div className="flex items-center ps-3">
+                    <input id={todo.id} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                    <label htmlFor={todo.name} className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        {todo.name}
+                    </label>
+                </div>
+                </li>
+                </>
+            ))}
+        </ul>
+    </>
+
 
   )
 }
