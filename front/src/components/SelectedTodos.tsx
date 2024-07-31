c9ab0507cfd8365245acb98bb1d4adabb88fbe1a
hukurou0 <junjun.0712.on@gmail.com>
2024-07-31 21:52:35 +0900

    add requiredTime

--- front/src/components/SelectedTodos.tsx
+++ front/src/components/SelectedTodos.tsx
@@ -7,14 +7,17 @@ export default function SelectedTodos() {
         {
             id:"1",
             name:"Vue JS",
+            requiredTime:5,
         },
         {
             id:"2",
             name:"React",
+            requiredTime:5,
         },
         {
             id:"3",
             name:"Angular",
+            requiredTime:5,
         },
     ]
 
@@ -24,10 +27,20 @@ export default function SelectedTodos() {
             <React.Fragment key={todo.id}>
                 <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                     <div className="flex items-center ps-3">
-                        <input id={todo.id} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                        <label htmlFor={todo.name} className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        <input
+                        id={todo.id}
+                        type="checkbox"
+                        value=""
+                        className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        />
+                        <div className="flex flex-col ms-3">
+                        <label htmlFor={todo.name} className="pt-1 text-sm font-medium text-gray-900 dark:text-gray-300">
                             {todo.name}
                         </label>
+                        <label htmlFor={todo.name} className="py-1 text-sm font-medium text-gray-400 dark:text-gray-300">
+                            {"推定所要時間:" + todo.requiredTime + "分"}
+                        </label>
+                        </div>
                     </div>
                 </li>
             </React.Fragment>
