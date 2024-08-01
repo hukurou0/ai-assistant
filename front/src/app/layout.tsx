c35ee409c9290b0af3f009ae0dec97d9f08c89f7
hukurou0 <junjun.0712.on@gmail.com>
2024-08-01 14:51:43 +0900

    remove unused page

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -23,8 +23,6 @@ export const metadata = {
 
 export const viewport = 'width=device-width, initial-scale=1';
 
-export const themeColor = '#000000';
-
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className={openSans.className}>
