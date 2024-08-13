eaf0f15053d83bc99cf8065d972b5f6108a772d6
hukurou0 <junjun.0712.on@gmail.com>
2024-08-13 21:39:21 +0900

    labelの位置が変わることでinput要素の大きさが変わる問題対応

--- front/src/components/SuggestTodoList.tsx
+++ front/src/components/SuggestTodoList.tsx
@@ -37,27 +37,30 @@ export default function SuggestTodoList(props: {id:string}) {
   }, []);
 
   return ( 
-    <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <ul className="w-70 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         {suggestTodos.map((data, index) => (
             <React.Fragment key={data.suggest_todo.id}>
                 <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                    <div className="flex items-center ps-3">
-                        <input
-                        id={data.suggest_todo.id}
-                        type="checkbox"
-                        value=""
-                        className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                        />
-                        <div className="flex flex-col ms-3">
-                        <label htmlFor={data.suggest_todo.title} className="pt-1 text-sm font-medium text-gray-900 dark:text-gray-300">
-                            {data.suggest_todo.title}
-                        </label>
-                        <label htmlFor={data.suggest_todo.title} className="py-1 text-sm font-medium text-gray-400 dark:text-gray-300">
-                            {"推定所要時間:" + data.suggest_todo.required_time + "分"}
-                        </label>
-                        </div>
-                    </div>
+                  <div className="flex items-center ps-3">
+                      <div className="flex-none w-6 mr-2">
+                          <input
+                              id={data.suggest_todo.id}
+                              type="checkbox"
+                              value=""
+                              className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                          />
+                      </div>
+                      <div className="flex flex-col flex-grow">
+                          <label htmlFor={data.suggest_todo.title} className="pt-1 text-sm font-medium text-gray-900 dark:text-gray-300">
+                              {data.suggest_todo.title}
+                          </label>
+                          <label htmlFor={data.suggest_todo.title} className="py-1 text-sm font-medium text-gray-400 dark:text-gray-300">
+                              {"推定所要時間:" + data.suggest_todo.required_time + "分"}
+                          </label>
+                      </div>
+                  </div>
                 </li>
+
             </React.Fragment>
         ))}
     </ul>
